d2b7bc3ef5c2709ec609e4058312394f6000b244
shinoburc <dandydot@gmail.com>
2013-07-30 13:47:18 +0900

    refactor

--- src/ExcelReplaceAll.java
+++ src/ExcelReplaceAll.java
@@ -2,33 +2,27 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import java.util.Properties;
 
 public class ExcelReplaceAll{
-    private Properties prop;
+    private static final String EXCEL_EXTENSION = ".xls";
+    private Properties prop = new Properties();
 
     public ExcelReplaceAll(String ini) throws Exception{
-        try {
-            this.prop = new java.util.Properties();
-            prop.load(new java.io.FileInputStream(ini));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        this.prop.load(new FileInputStream(ini));
     }
 
     public void execute() throws Exception{
         File input = new File(this.prop.getProperty("input-directory"));
         File output = new File(this.prop.getProperty("output-directory"));
         for(File file : input.listFiles(this.getXlsFilter())){
-            this.modify(file.getAbsolutePath(), output + "/" + file.getName());
+            this.replaceAll(file.getAbsolutePath(), output + "/" + file.getName());
         }
     }
 
-    public void modify(String input_file, String output_file) throws Exception{
+    public void replaceAll(String input_file, String output_file) throws Exception{
         POIFSFileSystem filein = new POIFSFileSystem(new FileInputStream(input_file));
         HSSFWorkbook wb = new HSSFWorkbook(filein);
 
@@ -58,7 +52,7 @@ public class ExcelReplaceAll{
     private FilenameFilter getXlsFilter() {
         return new FilenameFilter() {
             public boolean accept(File dir, String name) {
-                return name.matches(".*\\.xls");
+                return name.matches(".*\\" + ExcelReplaceAll.EXCEL_EXTENSION);
             }
         };
     }
